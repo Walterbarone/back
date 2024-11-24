d2cd16d9d460bc0e63c7dd29122fe8ee2c011278
Walter Barone <155363670+Walterbarone@users.noreply.github.com>
2024-11-24 12:09:25 -0300

    atualizando

--- server.js
+++ server.js
@@ -1,10 +1,50 @@
 import express from "express";
 
+const posts = [
+    {
+      id: 1,descricao: "Uma foto teste",
+      imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+      id: 2,descricao: "Gato fazendo yoga",
+      imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+      id: 3,descricao: "Cachorro sorrindo",
+      imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+      id: 4,descricao: "Paisagem montanhosa",
+      imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+      id: 5,descricao: "Comida deliciosa",
+      imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+      id: 6,descricao: "Citação inspiradora",
+      imagem: "https://placecats.com/millie/300/150"
+    }
+  ];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("servidor escutando...");
 });
 
-app.get("/api", (req, res) =>{
-    res.status(200). send("Boas vindas à imersão!");
+app.get("/posts", (req, res) =>{
+    res.status(200).json(posts);
+});
+
+function buscarPostPorID(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) =>{
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index]);
 });
